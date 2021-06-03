2ef00bee0b9608ad1808f814e1524079f6dd8dd7
WaterHashira <lakshay6632@gmail.com>
2021-06-04 03:11:49 +0530

    suggestion algo, blind mode

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 
 import 'package:flutter/services.dart';
 import 'package:smart_textbar/smart_textbar.dart';
+import 'package:smart_textbar/textbar.dart';
+
+import 'package:smart_textbar/ex.dart';
 
 
 
@@ -16,45 +20,28 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await SmartTextbar.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
+  //String _platformVersion = 'Unknown';
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+      home: Ex(),
+    );
+  }
+}
+
+class texttbar_example extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Center(
+          child: MyApp(),
         ),
       ),
     );
   }
 }
+
--- lib/textbar.dart
+++ lib/textbar.dart
@@ -1,163 +1,224 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:io';
-
-import 'package:flutter_tts/flutter_tts.dart';
-import 'package:flutter/services.dart';
-
 import 'dart:io' show Platform;
 import 'package:flutter/foundation.dart' show kIsWeb;
 
+import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
 class TextBar extends StatefulWidget {
+  String? speaker_language;
+  final double volume;
+  final double voice_pitch;
+  final double rate;
 
-  //TODO: MAKE ITS CONSTRUCTOR WITH ALL THE PROPERTIES OF  REGULAR TEXTFIELD:-
-  final String text;
-  final bool autoCorrect;
-  final bool autofocus;
-  final TextEditingController textController;
-  final Color cursorColor;
-  final double cursorHeight;
-  final Radius cursorRadius;
-  final double cursorWidth;
-  final TextStyle style;
-
-  var onChanged;
-
-  final InputDecoration decoration;
-  final DragStartBehavior dragStartBehavior;
-
-  TextBar({this.text ,this.autoCorrect , this.autofocus , this.textController , this.cursorColor , this.cursorHeight,
-    this.cursorRadius , this.cursorWidth ,this.style , this.onChanged ,this.decoration , this.dragStartBehavior});
+  TextBar({this.speaker_language = 'en-IN' , this.volume = 0.5 , this.voice_pitch = 1.0 , this.rate = 0.5});
 
   @override
   _TextBarState createState() => _TextBarState();
 }
 
+enum TtsState { playing, stopped, paused, continued }
+
 class _TextBarState extends State<TextBar> {
-  FlutterTts _flutterTts; //instance of flutter tts
-  bool is_playing = false; //to know if we need to start the speech or not
+  late FlutterTts flutterTts;
+  bool isCurrentLanguageInstalled = false;
+
+  TtsState ttsState = TtsState.stopped;
+
+  String? latest_word_in_textbar; //contains the latest word in the textbar after the latest space
+
+  get isPlaying => ttsState == TtsState.playing;
+  get isStopped => ttsState == TtsState.stopped;
+  get isPaused => ttsState == TtsState.paused;
+  get isContinued => ttsState == TtsState.continued;
+
+  bool get isIOS => !kIsWeb && Platform.isIOS;
+  bool get isAndroid => !kIsWeb && Platform.isAndroid;
+  bool get isWeb => kIsWeb;
 
   @override
-  void initState() {
+  initState() {
     super.initState();
-    initializeTts();
+    initTts();
   }
 
-  //method for initialising flutterTts platform handlers:-
-  initializeTts() {
-    _flutterTts = FlutterTts();
+  initTts() {
+    flutterTts = FlutterTts();
 
-    _flutterTts.setStartHandler(() {
+    if (isAndroid) {
+      _getEngines();
+    }
+
+    flutterTts.setStartHandler(() {
       setState(() {
-        print("playing");
-        is_playing = true;
+        print("Playing");
+        ttsState = TtsState.playing;
       });
     });
 
-    _flutterTts.setCompletionHandler(() {
+    flutterTts.setCompletionHandler(() {
       setState(() {
-        print("speech completed!");
-        is_playing = false;
+        print("Complete");
+        ttsState = TtsState.stopped;
       });
     });
 
-    _flutterTts.setErrorHandler((err) {
+    flutterTts.setCancelHandler(() {
       setState(() {
-        print("error occurred: " + err);
-        is_playing = false;
+        print("Cancel");
+        ttsState = TtsState.stopped;
       });
     });
-  }
 
-  //method for setting the speech language every time the speech is started :-
-  void setTtsLanguage() async {
-    await _flutterTts.setLanguage("en-US");
-  }
-
-  //method for selecting the male voice every time the speech is started :-
-  void speechSettings1() {
-    _flutterTts.setVoice({"name": "Karen", "locale": "en-AU"}); //TODO: CHANGE IT TO A MALE VOICE
-    _flutterTts.setPitch(1.5);
-    _flutterTts.setSpeechRate(.9);
-  }
-
-  //method for selecting the female voice every time the speech is started :-
-  void speechSettings2() async{
-    await _flutterTts.setVoice({"name": "Karen", "locale": "en-AU"});
-    _flutterTts.setPitch(1);
-    _flutterTts.setSpeechRate(0.5);
-  }
-
-  //method for starting the speech :-
-  Future _speak(String text) async {
-    if (text != null && text.isNotEmpty) {
-      var result = await _flutterTts.speak(text);
-      if (result == 1)
+    if (isWeb || isIOS) {
+      flutterTts.setPauseHandler(() {
         setState(() {
-          is_playing = true;
+          print("Paused");
+          ttsState = TtsState.paused;
         });
+      });
+
+      flutterTts.setContinueHandler(() {
+        setState(() {
+          print("Continued");
+          ttsState = TtsState.continued;
+        });
+      });
+    }
+
+    flutterTts.setErrorHandler((msg) {
+      setState(() {
+        print("error: $msg");
+        ttsState = TtsState.stopped;
+      });
+    });
+  }
+
+  Future<dynamic> _getLanguages() => flutterTts.getLanguages;
+
+  Future _getEngines() async {
+    var engines = await flutterTts.getEngines;
+    if (engines != null) {
+      for (dynamic engine in engines) {
+        print(engine);
+      }
     }
   }
 
-  //method for stopping the speech :-
-  Future _stop() async {
-    var result = await _flutterTts.stop();
-    if (result == 1)
-      setState(() {
-        is_playing = false;
-      });
+  Future _speak() async {
+    await flutterTts.setVolume(widget.volume);
+    await flutterTts.setSpeechRate(widget.rate);
+    await flutterTts.setPitch(widget.voice_pitch);
+
+    if (latest_word_in_textbar != null) {
+      if (latest_word_in_textbar!.isNotEmpty) {
+        await flutterTts.awaitSpeakCompletion(true);
+        await flutterTts.speak(latest_word_in_textbar!);
+      }
+    }
   }
 
+  Future _stop() async {
+    var result = await flutterTts.stop();
+    if (result == 1) setState(() => ttsState = TtsState.stopped);
+  }
+
+  Future _pause() async {
+    var result = await flutterTts.pause();
+    if (result == 1) setState(() => ttsState = TtsState.paused);
+  }
 
   @override
   void dispose() {
     super.dispose();
-    _flutterTts.stop();
+    flutterTts.stop();
   }
 
-  String textValue;
-  var ttsState;
+  List<DropdownMenuItem<String>> getLanguageDropDownMenuItems(
+      dynamic languages) {
+    var items = <DropdownMenuItem<String>>[];
+    for (dynamic type in languages) {
+      items.add(DropdownMenuItem(
+          value: type as String?, child: Text(type as String)));
+    }
+    return items;
+  }
 
-  String last_word; //latest word written in the textbar by the user
+  void changedLanguageDropDownItem(String? selectedType) {
+    setState(() {
+      widget.speaker_language = selectedType;
+      flutterTts.setLanguage(widget.speaker_language!);
+      if (isAndroid) {
+        flutterTts
+            .isLanguageInstalled(widget.speaker_language!)
+            .then((value) => isCurrentLanguageInstalled = (value as bool));
+      }
+    });
+  }
+
+  //method that triggers when text in textbar is changed:-
+  void _onChange(String text) {
+    setState(() {
+      latest_word_in_textbar = text;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: TextField(
-        autocorrect: widget.autoCorrect,
-        autofocus: widget.autofocus,
-        controller: widget.textController,
+    return MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text('Flutter TTS'),
+            ),
+            body: SingleChildScrollView(
+                scrollDirection: Axis.vertical,
+                child: Column(children: [
+                  _inputSection(),
+                  _futureBuilder(),
+                ]))));
+  }
 
-        cursorColor: widget.cursorColor,
-        cursorHeight: widget.cursorHeight,
-        cursorRadius: widget.cursorRadius,
-        cursorWidth: widget.cursorWidth,
+  Widget _futureBuilder() => FutureBuilder<dynamic>(
+      future: _getLanguages(),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.hasData) {
+          return _languageDropDownSection(snapshot.data);
+        } else if (snapshot.hasError) {
+          return Text('Error loading languages...');
+        } else
+          return Text('Loading Languages...');
+      });
 
-        style: widget.style,
-        onChanged: (value){
-          if(value != ' '){
-            setState(() {
-              //ttsState = value;
-              last_word = last_word + value;
-            });
+  Widget _inputSection() => Container(
+      alignment: Alignment.topCenter,
+      padding: EdgeInsets.only(top: 25.0, left: 25.0, right: 25.0),
+      child: TextField(
+        onChanged: (String value) {
+          if(value.endsWith(' ')){
+            _speak();
+            latest_word_in_textbar = null;
           }
           else{
-            setState(() async{
-              ttsState = last_word;
-              await setTtsLanguage();
-              speechSettings1(); //TODO: PROVIDE OPTION FOR CHANGING THE VOICE OF THE SPEAKER TO THE USER
-              await _speak(ttsState);
-              await _stop();
-            });
+            _stop();
+            latest_word_in_textbar = value;
           }
-          widget.onChanged;
-        },
-        decoration: widget.decoration,
-        dragStartBehavior: widget.dragStartBehavior,
-      ),
-    );
-  }
-}
 
+          _onChange(value);
+        },
+      ));
+
+
+  Widget _languageDropDownSection(dynamic languages) => Container(
+      padding: EdgeInsets.only(top: 50.0),
+      child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+        DropdownButton(
+          value: widget.speaker_language,
+          items: getLanguageDropDownMenuItems(languages),
+          onChanged: changedLanguageDropDownItem,
+        ),
+        Visibility(
+          visible: isAndroid,
+          child: Text("Is installed: $isCurrentLanguageInstalled"),
+        ),
+      ]));
+}
